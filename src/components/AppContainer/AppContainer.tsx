cb0e21d867b6f184125719f2c5b1fc00dc575773
GrzybowskiPiotr <grzybowski_piotr@hotmail.com>
2024-05-26 18:59:39 +0200

    propsWithChildren added

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,3 +1,9 @@
-export function AppContainer({ children }) {
+import { PropsWithChildren } from "react";
+
+type AppContainer = {
+	name: "";
+};
+
+export function AppContainer({ children }: PropsWithChildren<AppContainer>) {
 	return <div className="appContainer">{children}</div>;
 }
